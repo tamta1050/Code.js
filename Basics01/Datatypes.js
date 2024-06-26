7926d257d6ac29f7008a1ab106b1692c0d5bdb49
Rohit  Kumar Tamta <120511717+tamta1050@users.noreply.github.com>
2024-06-26 20:02:39 +0000

    datatypes

---
+++ Basics01/Datatypes.js
@@ -0,0 +1,32 @@
+ "use strict" // Treat all JS code as newer version
+
+//  alert(3 + 3) // We are using NodeJs,not browser
+
+console.log(3+3) 
+
+//  Code readibility should be high //
+
+console.log("Rohit");
+
+let name = "Rohit"
+let age = 22
+let isLoggedIn = false 
+
+// number => 2 to power 56
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object 
+
+
+
+    
+
