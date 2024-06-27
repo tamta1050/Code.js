6d291dda4d4c99ff087bb247eca009e304ad7fab
Rohit  Kumar Tamta <120511717+tamta1050@users.noreply.github.com>
2024-06-27 05:44:02 +0000

    conversionofdatatypes

--- Basics01/ConversionOperation.js
+++ Basics01/ConversionOperation.js
@@ -1,4 +1,23 @@
-let score = 67
+let score = false
 
 console.log(typeof score);
 console.log (typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+let someNumber = 43
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
